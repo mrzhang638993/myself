dcd91d15b25861c5eebe408c748c7a2b3089837e
mrzhang638993 <1446719386@qq.com>
2019-06-18 10:10:57 +0800

    commit  swagger

--- src/main/java/com/asiainfo/selfstudy/Controller/Test.java
+++ src/main/java/com/asiainfo/selfstudy/Controller/Test.java
@@ -3,6 +3,7 @@ package com.asiainfo.selfstudy.Controller;
 import com.asiainfo.selfstudy.interfaces.Add;
 import com.asiainfo.selfstudy.interfaces.Update;
 import com.asiainfo.selfstudy.vo.WorkInfoForm;
+import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
+@Api(value = "测试swagger以及sprngmvc验证功能的controller")
 public class Test {
 
 
---
+++ src/main/java/com/asiainfo/selfstudy/configuration/Config.java
@@ -0,0 +1,9 @@
+package com.asiainfo.selfstudy.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+
+}
